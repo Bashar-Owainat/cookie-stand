45e2b3dd61c3bd35ac39faf8b4035c3462e16af3
Bashar-Owainat <owainatbashar13@gmail.com>
2021-08-11 21:40:12 +0300

    added form

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 'use strict';
 
-let hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 let randomCustomersAvg = [];
-let cookies = [];
-let total =0;
+// let cookies = [];
+let total = 0;
 let container = document.getElementById('container');
 let tableEl = document.createElement('table');
 container.appendChild(tableEl);
-let totals = [];
+let cities = [];
 
-function City(cityName, minCust, maxCust,avgCookieSale ){
+
+function City(cityName, minCust, maxCust, avgCookieSale) {
     this.cityName = cityName;
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgCookieSale = avgCookieSale;
     this.randomCustomersAvg = randomCustomersAvg;
-    this.cookies = cookies;
+    this.cookies = [];
     this.total = total;
-    this.totals = totals;
+    cities.push(this);
 }
 
 City.prototype.randomAvgCust = function () {
     let min;
     let max;
-    for(let i = 0; i < hours.length ; i++ ){
+    for (let i = 0; i < hours.length; i++) {
         min = Math.ceil(this.minCust);
         max = Math.floor(this.maxCust);
         let randomNum = Math.floor(Math.random() * (max - min + 1) + min);
@@ -34,9 +35,9 @@ City.prototype.randomAvgCust = function () {
 
 
 
-City.prototype.avgCookies = function(){
-    for(let i = 0; i < hours.length; i++){
-        this.cookies[i]= Math.ceil(randomCustomersAvg[i] * this.avgCookieSale);
+City.prototype.avgCookies = function () {
+    for (let i = 0; i < hours.length; i++) {
+        this.cookies[i] = Math.ceil(randomCustomersAvg[i] * this.avgCookieSale);
         this.total += this.cookies[i];
     }
     this.cookies[14] = this.total;
@@ -45,8 +46,7 @@ City.prototype.avgCookies = function(){
 
 
 
-
-City.prototype.render = function(){
+City.prototype.render = function () {
     let trEl = document.createElement('tr');
     tableEl.appendChild(trEl);
 
@@ -54,78 +54,28 @@ City.prototype.render = function(){
     trEl.appendChild(tdEl);
     tdEl.textContent = this.cityName;
 
-    for(let i = 0; i <= hours.length; i++){
+    for (let i = 0; i <= hours.length; i++) {
         let tdEl2 = document.createElement('td');
         trEl.appendChild(tdEl2);
-        tdEl2.textContent = cookies[i];
+        tdEl2.textContent = this.cookies[i];
     }
-
 
 };
 
-function createHeader(){
+function createHeader() {
     let trEl = document.createElement('tr');
     tableEl.appendChild(trEl);
 
     let thEl1 = document.createElement('th');
     trEl.appendChild(thEl1);
     thEl1.textContent = '';
+    for (let i = 0; i < hours.length; i++) {
+        let thEl2 = document.createElement('th');
+        trEl.appendChild(thEl2);
+        thEl2.textContent = hours[i];
 
-    let thEl2 = document.createElement('th');
-    trEl.appendChild(thEl2);
-    thEl2.textContent = '6am';
 
-    let thEl3 = document.createElement('th');
-    trEl.appendChild(thEl3);
-    thEl3.textContent = '7am';
-
-    let thEl4 = document.createElement('th');
-    trEl.appendChild(thEl4);
-    thEl4.textContent = '8am';
-
-    let thEl5 = document.createElement('th');
-    trEl.appendChild(thEl5);
-    thEl5.textContent = '9am';
-
-    let thEl6 = document.createElement('th');
-    trEl.appendChild(thEl6);
-    thEl6.textContent = '10am';
-
-    let thEl7 = document.createElement('th');
-    trEl.appendChild(thEl7);
-    thEl7.textContent = '11am';
-
-    let thEl8 = document.createElement('th');
-    trEl.appendChild(thEl8);
-    thEl8.textContent = '12pm';
-
-    let thEl9 = document.createElement('th');
-    trEl.appendChild(thEl9);
-    thEl9.textContent = '1pm';
-
-    let thEl10 = document.createElement('th');
-    trEl.appendChild(thEl10);
-    thEl10.textContent = '2pm';
-
-    let thEl11 = document.createElement('th');
-    trEl.appendChild(thEl11);
-    thEl11.textContent = '3pm';
-
-    let thEl12 = document.createElement('th');
-    trEl.appendChild(thEl12);
-    thEl12.textContent = '4pm';
-
-    let thEl13 = document.createElement('th');
-    trEl.appendChild(thEl13);
-    thEl13.textContent = '5pm';
-
-    let thEl14 = document.createElement('th');
-    trEl.appendChild(thEl14);
-    thEl14.textContent = '6pm';
-
-    let thEl15 = document.createElement('th');
-    trEl.appendChild(thEl15);
-    thEl15.textContent = '7pm';
+    }
 
     let thEl16 = document.createElement('th');
     trEl.appendChild(thEl16);
@@ -134,7 +84,7 @@ function createHeader(){
 
 createHeader();
 
-function createFooter(){
+function createFooter() {
 
     let trEl = document.createElement('tr');
     tableEl.appendChild(trEl);
@@ -142,14 +92,30 @@ function createFooter(){
     let tdEl = document.createElement('th');
     trEl.appendChild(tdEl);
     tdEl.textContent = 'Totals';
+    let totalOftotal = 0;
+    for (let i = 0; i < hours.length; i++) {
+        let totalHourly = 0;
+        for (let j = 0; j < cities.length; j++) {
+            totalHourly += cities[j].cookies[i];
+            totalOftotal += cities[j].cookies[i];
+
+
+        }
+        let tdEl2 = document.createElement('th');
+        trEl.appendChild(tdEl2);
+        tdEl2.textContent = totalHourly;
+    }
+    let tdEl2 = document.createElement('th');
+    trEl.appendChild(tdEl2);
+    tdEl2.textContent = totalOftotal;
 
 }
 
-let seattle = new City('seattle',23,65,6.3);
-let tokyo = new City('tokyo',3,24,61.2);
-let dubai = new City('dubai',11,38,3.7);
-let paris = new City('paris',20,38,2.3);
-let lami = new City('lami',2,16,4.6);
+let seattle = new City('seattle', 23, 65, 6.3);
+let tokyo = new City('tokyo', 3, 24, 61.2);
+let dubai = new City('dubai', 11, 38, 3.7);
+let paris = new City('paris', 20, 38, 2.3);
+let lami = new City('lami', 2, 16, 4.6);
 
 
 
@@ -173,6 +139,22 @@ lami.randomAvgCust();
 lami.avgCookies();
 lami.render();
 
-createFooter();
 
+
+let info = document.getElementById('info');
+info.addEventListener('submit', addBranch);
+function addBranch(event){
+    event.preventDefault();
+    // let pEl = document.createElement('p');
+    let cityName = event.target.cityName.value;
+    let minCust = event.target.minCust.value;
+    let maxCust = event.target.maxCust.value;
+    let avgCookieSale = event.target.avgCookieSale.value;
+    let newBranch = new City(cityName, minCust, maxCust, avgCookieSale);
+    newBranch.randomAvgCust();
+    newBranch.avgCookies();
+    newBranch.render();
+    createFooter();
+}
+createFooter();
 
